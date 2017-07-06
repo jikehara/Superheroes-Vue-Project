6e693bdcd88c316680148f789b1d150e2bd861cb
root <root@Naptop.localdomain>
2017-07-06 09:39:42 -0700

    Added some small changes to server.js, updated post and get routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var superHero = require("./models/Superheroes");
+var SuperHero = require("./models/Superheroes");
 var app = express();
 var port = 3003;
 
@@ -11,11 +11,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function(req, res) {
-  res.json({ message: "Hello World" });
+  SuperHero.find(function(err, superheroes) {
+    if (err) throw err;
+    res.json({ data: superheroes });
+  });
 });
 
 app.post("/", function(req, res) {
-  var superhero = new superHero();
+  res.send("Hello Post");
+  var superhero = new SuperHero();
   superhero.name = req.body.name;
   superhero.superpower = req.body.superpower;
   superhero.save(function(superhero) {
