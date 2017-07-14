a5c85364160ab1bdbd4a85f65c3760badb0ac290
root <root@Naptop.localdomain>
2017-07-14 11:17:29 -0700

    Added all the methods to villain.js as we have in hero.js

--- public/villain.js
+++ public/villain.js
@@ -5,6 +5,49 @@ var app = new Vue({
   data: {
     title: title,
     villains: undefined,
-
+    postTIlte: "Create a Villain!",
+    name: undefined,
+    evilPower: undefined,
+    img: undefined
+  },
+  created: function() {
+    this.fetchData();
+  },
+  methds: {
+    fetchData: function() {
+      var self = this;
+      $.ajax({
+        method: "GET",
+        url:"/api/villains"
+      }).done(function(response) {
+        self.villains = response.data;
+        console.log("Received villains," response);
+      });
+    },
+    postVillain: function() {
+      var self = this;
+      var newVillain = {
+        name: this.name,
+        evilPower: this.name,
+        img: this.img
+      };
+      $.ajax({
+        url: "/api/villains",
+        method: "POST",
+        data: newVillain
+      }).done(function(response) {
+        console.log(response.data, "Villain created.");
+      });
+    },
+    deleteVillain: function(_id) {
+      console.log("Deleting villain", _id);
+      var self = this;
+      $.ajax({
+        method: "DELETE",
+        url: "/api/villains/"+_id
+      }).done(function(rsponse) {
+        console.log(response);
+      });
+    }
   }
 })
